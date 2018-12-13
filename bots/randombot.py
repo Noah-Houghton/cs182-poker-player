cd4a7d8b9581cf8578e404f9cd39237f0fbb8657
Noah Houghton <noah.houghton.97@gmail.com>
2018-12-13 17:03:08 -0500

    added bots, template for bots, more printing in simulate

--- bots/randombot.py
+++ bots/randombot.py
@@ -1,30 +1,48 @@
 from pypokerengine.players import BasePokerPlayer
-import random
+import random as rand
 
 class RandomBot(BasePokerPlayer):
-    def declare_action(self, valid_actions, hole_card, round_state):
-        # valid_actions format => [raise_action_info, call_action_info, fold_action_info]
-        random_action_info = random.choice(valid_actions)
-        action = random_action_info["action"]
-        if action == 'raise':
-            amount = random.randint(random_action_info["amount"]["min"], random_action_info["amount"]["max"])
-        else:
-            amount = random_action_info["amount"]
-        return action, amount   # action returned here is sent to the poker engine
 
-    def receive_game_start_message(self, game_info):
-        pass
+  def __init__(self):
+    self.fold_ratio = self.call_ratio = raise_ratio = 1.0/3
 
-    def receive_round_start_message(self, round_count, hole_card, seats):
-        pass
+  def set_action_ratio(self, fold_ratio, call_ratio, raise_ratio):
+    ratio = [fold_ratio, call_ratio, raise_ratio]
+    scaled_ratio = [ 1.0 * num / sum(ratio) for num in ratio]
+    self.fold_ratio, self.call_ratio, self.raise_ratio = scaled_ratio
 
-    def receive_street_start_message(self, street, round_state):
-        pass
+  def declare_action(self, valid_actions, hole_card, round_state):
+    choice = self.__choice_action(valid_actions)
+    action = choice["action"]
+    amount = choice["amount"]
+    if action == "raise":
+      amount = rand.randrange(amount["min"], max(amount["min"], amount["max"]) + 1)
+    return action, amount
 
-    def receive_game_update_message(self, action, round_state):
-        pass
+  def __choice_action(self, valid_actions):
+    r = rand.random()
+    if r <= self.fold_ratio:
+      return valid_actions[0]
+    elif r <= self.call_ratio:
+      return valid_actions[1]
+    else:
+      return valid_actions[2]
 
-    def receive_round_result_message(self, winners, hand_info, round_state):
-        pass
+
+  def receive_game_start_message(self, game_info):
+    pass
+
+  def receive_round_start_message(self, round_count, hole_card, seats):
+    pass
+
+  def receive_street_start_message(self, street, round_state):
+    pass
+
+  def receive_game_update_message(self, new_action, round_state):
+    pass
+
+  def receive_round_result_message(self, winners, hand_info, round_state):
+      pass
+
 def setup_ai():
     return RandomBot()
