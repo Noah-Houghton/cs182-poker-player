08018fa534c4e21c262ba0ff453a889e9f59835f
Noah Houghton <noah.houghton.97@gmail.com>
2018-12-18 03:01:09 -0500

    add'l instructions

--- automatetests.py
+++ automatetests.py
@@ -4,6 +4,14 @@ import simulate
 """
 Populate "tests.txt" with the flags you want to run `python simulate.py` with.
 Then run python `automatetests.py`.
+
+Example format for `tests.txt`:
+-p SarsaBot -o NaiveBot -n 1 -g 10 -a 5 -s 500 -r 50 -m 15 -t 10
+
+Note that running automatetests.py will APPEND the output of the tests to the existing files.
+Make sure to delete any tests you don't want to re-run from `tests.txt` before launching the program, or delete
+them in `XXXbot.txt` to prevent logging the data twice!
+
 """
 
 def main():
