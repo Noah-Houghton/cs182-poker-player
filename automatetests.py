3d4bfce99e7bdcfbf0fce2fc860be0030092df47
Noah Houghton <noah.houghton.97@gmail.com>
2018-12-18 03:07:18 -0500

    added commenting support for tests.txt

--- automatetests.py
+++ automatetests.py
@@ -19,9 +19,10 @@ def main():
     with open("tests.txt", "r") as input:
         argslist = input.readlines()
     for args in argslist:
-        print("Beginning test {}".format(testNum))
-        simulate.main(args.split()+["-w"])
-        testNum += 1
+        if not args.startswith("#"):
+            print("Beginning test {}".format(testNum))
+            simulate.main(args.split()+["-w"])
+            testNum += 1
 
 if __name__ == '__main__':
     main()
